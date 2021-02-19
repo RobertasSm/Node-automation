0aa770c74ab20b766bd5f92509461801888d49a7
robiqaz <robiqaz@gmail.com>
2021-02-19 13:32:17 +0000

    Created django role and tweaked deploy,firewall,nginx scripts
    Final commit as time has ran out.

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,7 @@ echo "password: "$3
 sshpass -p $3 scp -p $ssh_params ./init/init.sh $2@$1:/tmp/init.sh
 sshpass -p $3 scp -p $ssh_params ~/.ssh/id_rsa.pub $2@$1:~/authorized_keys
 sshpass -p $3 ssh $ssh_params $2@$1 'echo '$3' | sudo -S bash /tmp/init.sh '$2
+cd ansible
+ansible-playbook -e strategy=linear --ssh-extra-args='-o StrictHostKeyChecking=no' -i inventories/dev playbooks/deploy.yml -i $1,
 
 fi
